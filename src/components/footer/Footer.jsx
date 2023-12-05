a66a35c5b4d2697211a78bb3ba330ed0c666cc37
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2023-12-05 18:51:37 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <div className=" bg-gray-900 py-4">
+            <h2 className=' text-white text-center font-light'>Made with By Knus</h2>
+        </div>
+    </div>
+  )
+}
